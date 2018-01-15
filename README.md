2fa7e1d84215216a323c504b8a40128b74bba375
CY-nb <31556438+c3p0cy@users.noreply.github.com>
2018-01-15 14:11:08 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+Demo project for Spring Boot with @Profile
+----------------------------------------------------
+###### References:
+* [Spring Boot Profiles example](https://www.mkyong.com/spring-boot/spring-boot-profiles-example/)
---
+++ src/main/java/tw/c3p0cy/practice/springboot/springbootprofiles/SpringBootProfilesApplication.java
@@ -0,0 +1,12 @@
+package tw.c3p0cy.practice.springboot.springbootprofiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProfilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProfilesApplication.class, args);
+	}
+}
